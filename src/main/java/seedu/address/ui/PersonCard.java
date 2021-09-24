77ad0d342275a02547f32b8abc5f46f186fd2c95
Frederick Pek <ssixthlhm@gmail.com>
2021-09-24 18:39:34 +0800

    Update gui icons and style

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -54,7 +54,7 @@ public class PersonCard extends UiPart<Region> {
         email.setText(person.getEmail().value);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
-                .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+                .forEach(tag -> tags.getChildren().add(new Label("Case #" + tag.tagName)));
     }
 
     @Override
