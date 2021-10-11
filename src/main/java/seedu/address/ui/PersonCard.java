35c7fed9ea4f02c79e499c7384596bc1fae8b9fb
Frederick Pek <ssixthlhm@gmail.com>
2021-10-11 21:57:50 +0800

    Fix CheckStyle for PersonCard

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -122,19 +122,33 @@ public class PersonCard extends UiPart<Region> {
         node.setVisible(false);
     }
 
-    public HBox getWorkAddressHBox() { return workAddressHBox; }
+    public HBox getWorkAddressHBox() {
+        return workAddressHBox;
+    }
 
-    public HBox getQuarantineAddressHBox() { return quarantineAddressHBox; }
+    public HBox getQuarantineAddressHBox() {
+        return quarantineAddressHBox;
+    }
 
-    public HBox getShnPeriodHBox() { return shnPeriodHBox; }
+    public HBox getShnPeriodHBox() {
+        return shnPeriodHBox;
+    }
 
-    public HBox getNextOfKinNameHBox() { return nextOfKinNameHBox; }
+    public HBox getNextOfKinNameHBox() {
+        return nextOfKinNameHBox;
+    }
 
-    public HBox getNextOfKinPhoneHBox() { return nextOfKinPhoneHBox; }
+    public HBox getNextOfKinPhoneHBox() {
+        return nextOfKinPhoneHBox;
+    }
 
-    public HBox getNextOfKinAddressHBox() { return nextOfKinAddressHBox; }
+    public HBox getNextOfKinAddressHBox() {
+        return nextOfKinAddressHBox;
+    }
 
-    public VBox getNextOfKinBlock() { return nextOfKinBlock; }
+    public VBox getNextOfKinBlock() {
+        return nextOfKinBlock;
+    }
 
     @Override
     public boolean equals(Object other) {
